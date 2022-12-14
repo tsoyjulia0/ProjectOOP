881050ed19697d758b55ba8709c7c8f3f2fba189
DaurenSh <98632310+DaurenSh@users.noreply.github.com>
2022-12-14 20:14:36 +0600

    Update LessonExt.java

--- Controller/LessonExt.java
+++ Controller/LessonExt.java
@@ -1,12 +1,19 @@
 package Controller;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class LessonExt {
 	protected HashMap<Integer, Boolean> attendance;
 	
 	public void setAttendance(HashMap<Integer, Boolean> att) {
 		this.attendance=att;
+	}
+	
+	public void getAttendance() {
+		for (Entry<Integer, Boolean> entry : this.attendance.entrySet()) {
+            System.out.println(entry.getValue()+" "+ entry.getKey());
+        }
 	}
 	
 	public void setAttendance(int id, boolean bool) {
