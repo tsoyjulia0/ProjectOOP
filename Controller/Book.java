6f638e6dae4b084a38be382d5132fd320474b177
tsoyjulia0 <98633575+tsoyjulia0@users.noreply.github.com>
2022-12-17 00:48:19 +0600

    Add files via upload

--- Controller/Book.java
+++ Controller/Book.java
@@ -1,5 +1,32 @@
 package Controller;
 
 public class Book {
-
+	private int id;
+	private String name;
+	{
+		DataBase.addBook(this);
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Book(String name,int id) {
+		this.id=id;
+		this.name=name;
+		
+	}
+	public Book() {
+		
+	}
+	public String toString() {
+		return id+" "+name+"\n";
+	}
 }
---
+++ Controller/LibrarianController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import Model.LibrarianModel;
+import Model.StudentModel;
+
+public class LibrarianController {
+	private LibrarianModel librarian;
+	public LibrarianController(LibrarianModel l) {
+		librarian=l;
+	}
+	public void giveBook(StudentModel s, String bookName) {
+		
+	}
+}
