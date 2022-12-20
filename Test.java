3a4921f2993cd097f09508ed8284af5a68ea2090
tsoyjulia0 <98633575+tsoyjulia0@users.noreply.github.com>
2022-12-20 14:28:42 +0600

    Update Test.java

--- Test.java
+++ Test.java
@@ -7,9 +7,12 @@ import Controller.DataBase;
 import Controller.Faculty;
 import Controller.ManagerController;
 import Controller.Mark;
+import Controller.Researcher;
+import Controller.SimpleResearcher;
 import Controller.StudentController;
 import Controller.TeacherController;
 import Controller.UserController;
+import Controller.WithResearch;
 import Model.AdminModel;
 import Model.Degree;
 import Model.EmployeeModel;
@@ -33,28 +36,15 @@ public class Test {
 		TeacherModel t2 = new TeacherModel(2,"T2202", "pass2", "Pakizar", "Shamoi", Gender.F, TeachDegree.L);
 		TeacherModel t3 = new TeacherModel(3,"T2203", "pass3", "Aibek", "Kuralbayev", Gender.M, TeachDegree.L);
 		TeacherModel t4 = new TeacherModel(4,"T2204", "pass4", "Elena", "Li", Gender.F, TeachDegree.L);
-		TeacherModel t5 = new TeacherModel(5,"T2205", "pass5", "Akerke", "Nasyr", Gender.M, TeachDegree.T);
+		TeacherModel t5 = new TeacherModel(51,"T2205", "pass5", "Akerke", "Nasyr", Gender.M, TeachDegree.T);
 		TeacherModel t6 = new TeacherModel(6,"T2206", "pass6", "Chardon", "Gaetoen", Gender.M, TeachDegree.T);
 		TeacherModel t7 = new TeacherModel(7,"T2207", "pass7", "Dmitriy", "Sidorov", Gender.M, TeachDegree.T);
 		TeacherModel t8 = new TeacherModel(8,"T2208", "pass8", "Anatoliy", "Odintsov", Gender.M, TeachDegree.L);
 		TeacherModel t9 = new TeacherModel(9,"T2209", "pass9", "Alimznan", "Amanov", Gender.M, TeachDegree.L);
 		TeacherModel t10 = new TeacherModel(10,"T22010", "pass10", "Bobur", "Muksimbayev", Gender.M, TeachDegree.L);
 		
-
-
-//		StudentModel s1=new StudentModel("a_alenov@kbtu.kz","jsjsjd","Abay",Gender.M);
-//	    StudentModel s2=new StudentModel("y_tsoy@kbtu.kz","aazzz","Yuliya",Gender.F);
-//	    StudentModel s3=new StudentModel("d_dauren@kbtu.kz","fxxxz","Dauren",Gender.F);
-//	    StudentModel s4=new StudentModel("t_tempo@kbtu.kz","asdsw","Tempo",Gender.F);
-//	    StudentModel s5=new StudentModel("asd_sdawe@kbtu.kz","vxcvcer","ASRQWS",Gender.M);
-//	    StudentModel s6=new StudentModel("fsf_asdasx@kbtu.kz","wetsgs","FVVd",Gender.M);
-//	    StudentModel s7=new StudentModel("aeqwed__sdmar@kbtu.kz","bbbcxd","Madi",Gender.M);
-//	    StudentModel s8=new StudentModel("aweiifj_fjaf@kbtu.kz","sdasweq","Damir",Gender.F);
-//	    StudentModel s9=new StudentModel("fmrjr_asd@kbtu.kz","wqgsv","Mariya",Gender.F);
-//	    StudentModel s10=new StudentModel("dac_qwee@kbtu.kz","fffsa","Hyi",Gender.M);
-	    
 		StudentModel s1=new StudentModel(21030751,"a_alenov@kbtu.kz","jsjsjd","Abay","Alenov",Gender.M, 4); 
-	    StudentModel s2=new StudentModel(21876575,"y_tsoy@kbtu.kz","aazzz","Юля","Tsoi",Gender.F, 1); 
+	    StudentModel s2=new StudentModel(21876575,"y_tsoy@kbtu.kz","aazzz","Yuliya","Tsoy",Gender.F, 1); 
 	    StudentModel s3=new StudentModel(217643474,"d_dauren@kbtu.kz","fxxxz","Dauren","Haver",Gender.F, 2); 
 	    StudentModel s4=new StudentModel(216467545,"t_tempo@kbtu.kz","asdsw","Tempo","Kruger",Gender.F, 1); 
 	    StudentModel s5=new StudentModel(216643644,"asd_sdawe@kbtu.kz","vxcvcer","ASRQWS","Wipsow",Gender.M, 3); 
@@ -92,11 +82,10 @@ public class Test {
 	    CourseInfo c3 = new CourseInfo(3, "PP2", "PP1", 5);
 		CourseInfo c4 = new CourseInfo(4, "ADS", null, 5);
 		
-		Course cr1 = new Course(c1, t1, students );
-		Course cr2 = new Course(c2, t2, students);
-		Course cr3 = new Course(c3, t4, students);
-		Course cr4 = new Course(c4, t9, students);
-   
+		Course cr1 = new Course(c1, t1);
+		Course cr2 = new Course(c2, t2);
+		Course cr3 = new Course(c3, t4);
+		Course cr4 = new Course(c4, t9);
 	
 		AdminModel admin = new AdminModel(101, "A2201", "Apass1", "Zhaniya", "Sadykova", Gender.F);
 		DataBase db = new DataBase();
@@ -122,12 +111,13 @@ public class Test {
 		LessonModel l2 = new LessonModel(cr2, t3, start2 , end2, 20, LessonType.P );
 		LessonModel l3 = new LessonModel(cr3, t4, start3 , end3, 90, LessonType.L );
 		
-		Mark mark1=new Mark(cr1,20,30,50,s1,t1); 
-		Mark mark2=new Mark(cr2,22,32,52,s2,t2); 
-		Mark mark3=new Mark(cr3,25,26,51,s3,t3); 
-		Mark mark4=new Mark(cr4,19,29,48,s4,t4);
+//		Mark mark1=new Mark(cr1,20,30,50,s1,t1); 
+//		Mark mark2=new Mark(cr2,22,32,52,s2,t2); 
+//		Mark mark3=new Mark(cr3,25,26,51,s3,t3); 
+//		Mark mark4=new Mark(cr4,19,29,48,s4,t4);
 		
 		TeacherController tc1 = new TeacherController(t1);
+		TeacherController tc5 = new TeacherController(t5);
 		AdminController ad1 = new AdminController(admin);
 		
 		StudentController st1 = new StudentController(s1);
@@ -150,7 +140,6 @@ public class Test {
 		System.out.println();
 		System.out.println(DataBase.getCourses());
 		st1.registerCourse(cr4);
-		mn.approveReg(s1, cr4);
 		st1.viewCourses();
 		
 		System.out.println(s1.getCntCredits());
@@ -160,16 +149,35 @@ public class Test {
 		System.out.println(s1.showDiplomaInfo());
 		
 		AdminController ac = new AdminController(admin);
-		ac.addStudent(s31);
+		ac.addStudent(s2);
+		System.out.println("Added:");
 		System.out.println(db.getStudents(m1));
-		ac.removeStudent(s31);
+		
+		ac.updateStudent(s2, "login", "newLogin");
+		System.out.println("Updated:");
 		System.out.println(db.getStudents(m1));
 		
+		ac.removeStudent(s2);
+		System.out.println("Removed:");
+		System.out.println(db.getStudents(m1));
 		
-//		tc1.putMark(s11, 30, "1Att");
-//		st11.viewMarks();
+		System.out.println();
 		
+		tc1.sendMessage("Good afternoon! Please, come to 425 room!", 51 );
+		tc5.showAllMessages();
 		
+		st1.rateTeacher(t9, 10);
+		System.out.println(cr4.getTeacher());
+		t9.viewRate();
+		
+		System.out.println();
+		
+		System.out.println(t1.getName() + " " + "Courses: ");
+		tc1.viewCourses();
+		
+		Researcher r = new SimpleResearcher("FirstResearch", "Analyze");
+		r = new WithResearch(r, s2);
+		System.out.println(r.doResearch());
 
 	}
 
