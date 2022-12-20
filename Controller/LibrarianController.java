b20b27e6e8d37e2958c059a72b51c7a210d2c0a7
Onelvay <98649055+Onelvay@users.noreply.github.com>
2022-12-20 21:26:20 +0600

    Update LibrarianController.java

--- Controller/LibrarianController.java
+++ Controller/LibrarianController.java
@@ -1,7 +1,10 @@
 package Controller;
 
+import java.util.Vector;
+
 import Model.LibrarianModel;
 import Model.StudentModel;
+import Model.UserModel;
 
 public class LibrarianController {
 	private LibrarianModel librarian;
@@ -10,5 +13,45 @@ public class LibrarianController {
 	}
 	public void giveBook(StudentModel s, String bookName) {
 		
+	}
+	public int getNumOfBooks() {
+		return DataBase.getBooks().size();
+	}
+	public int getNumOfBooks(String name) {
+		return DataBase.getBooks(name).size();
+	}
+	public boolean giveBook(UserModel user, String name) {
+		Vector<Book> books=DataBase.getBooks(name);
+		if(books.size()==0) {
+			System.out.println("Нету книг с таким названием");
+			return false;
+		}
+		else {
+			for(Book book:books) {
+				if(book.getOwner()==null) {
+					book.setOwner(user);
+					System.out.println("Успешно выдана книга "+book);
+					return true;
+				}
+			}
+			System.out.println("Не осталось книг");
+			return false;
+		}
+	}
+	public boolean returnBook(UserModel user,String name) {
+		Vector<Book> books=DataBase.getBooks(name);
+		if(books.size()==0) {
+			System.out.println("Нету книг с таким названием");
+			return false;
+		}
+		for(Book book: books) {
+			if(book.getOwner()==user) {
+				book.removeOwner();
+				System.out.println("Вы успешно вернули книгу");
+				return true;
+			}
+		}
+		System.out.println("Вы не должны книгу с таким названием");
+		return false;
 	}
 }
