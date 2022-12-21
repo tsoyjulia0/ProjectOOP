e748e1336a92e8c7444f8b60002379a6df72dbb8
tsoyjulia0 <98633575+tsoyjulia0@users.noreply.github.com>
2022-12-21 13:52:24 +0600

    Update StudentModel.java

--- Model/StudentModel.java
+++ Model/StudentModel.java
@@ -1,6 +1,8 @@
 package Model;
 
+import java.io.Serializable;
 import java.util.HashMap;
+import java.util.Set;
 import java.util.Vector;
 
 import Controller.Book;
@@ -10,25 +12,32 @@ import Controller.DiplomaProject;
 import Controller.Faculty;
 import Controller.Mark;
 import Controller.Organization;
+import Controller.Researcher;
 
-public class StudentModel extends UserModel {
-
+public class StudentModel extends UserModel implements Serializable {
+//    private Integer rate;
     private Integer yearOfStudy;
     private Degree degree;
     public int cntCredits = 0;
     private String org;
-    private Vector<Course> courses;
+    private Vector<Book> books;
+//    private Vector<Course> courses; //c
     private Organization organization;
     private Faculty faculty;
-    private Vector<Mark> marks;
+//    private Vector<Mark> marks;  //c
     public HashMap<Course, Mark> grades;
     private DiplomaProject diploma;
+    private double gpa = 0;
+    private Vector<Course> finished=new Vector<Course>();
     
     
-    
+    public Vector<Course> getFinishedCourses(){
+    	return finished;
+    }
     {
-    	marks = new Vector<Mark>();
-    	courses = new Vector<Course>();
+    	
+    	books = new Vector<Book>();
+    	grades = new HashMap<Course, Mark>();
     	DataBase.addStudent(this);
     }
    
@@ -41,7 +50,11 @@ public class StudentModel extends UserModel {
   
     }
     
-    public Vector<Mark> getMarks(){
+    public Set<Mark> getMarks(){
+    	Set<Mark> marks = (Set<Mark>) grades.values();
+//    	for(Course key: grades.keySet()){
+//    	    System.out.println(key + ": " + grades.get(key));
+//    	}
     	return marks;
     }
     
@@ -94,6 +107,15 @@ public class StudentModel extends UserModel {
     public void setFaculty(Faculty faculty) {
         this.faculty = faculty;
     }
+
+    public Vector<Book> getBooks() {
+        return this.books;
+    }
+
+    public void setBooks(Vector<Book> books) {
+        this.books = books;
+    }
+
     public DiplomaProject getDiploma() {
         return this.diploma;
     }
@@ -106,12 +128,20 @@ public class StudentModel extends UserModel {
         this.diploma = diploma;
     }
     
-    public Vector<Course> getCourses() {
-        return this.courses;
+    public Set<Course> getCourses() {
+//    	Set<Course> keys = grades.keySet();
+    	// To get all key: value
+    	Set<Course> courses = (Set<Course>) grades.keySet();
+//    	for(Course key: grades.keySet()){
+//    	    System.out.println(key + ": " + grades.get(key));
+//    	}
+    	return courses;
     }
 
     public void addCourse(Course course) {
-        courses.add(course);
+    	Mark m = new Mark();
+    	grades.put(course, m);
+//        courses.add(course);
         this.cntCredits += course.getNumOfCred();
     }
     
@@ -136,6 +166,7 @@ public class StudentModel extends UserModel {
     public Faculty getFac() {
         return this.faculty;
     }
+
 
 
     
